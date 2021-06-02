a5423d2bf4ff458b713336fd72122a8a0623d233
Alkis Gotovos <alkisg@mit.edu>
2021-06-02 15:22:51 -0400

    Cleanup for supplement

--- cpp/profile.sh
+++
@@ -1,3 +0,0 @@
-g++ -std=c++17 -O3 -ffast-math -flto -g -o prof prof.cpp
-valgrind --tool=callgrind --dump-instr=yes --collect-jumps=yes ./prof
-kcachegrind
--- init.sh
+++
@@ -1,3 +0,0 @@
-source "./miniconda/etc/profile.d/conda.sh"
-conda activate test_env
-export OMP_NUM_THREADS=1

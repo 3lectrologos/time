d0b45dc46e7cdadfbd50e8b1f12090552f3db55e
Alkis Gotovos <alkisg@mit.edu>
2021-06-02 15:33:14 -0400

    Small fix

--- setup.sh
+++ setup.sh
@@ -1,5 +1,3 @@
-sudo apt install cmake emacs build-essential unzip
-
 wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh
 bash miniconda.sh -b -p ./miniconda
 source "./miniconda/etc/profile.d/conda.sh"

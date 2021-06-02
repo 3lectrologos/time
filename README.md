54732c2440ef95aec0f838f3b97a94ef3fbdf101
Alkis Gotovos <alkisg@mit.edu>
2021-06-02 15:30:45 -0400

    Readme and setup script

---
+++ README.md
@@ -0,0 +1,10 @@
+Requirements
+------------
+
+CMake, unzip, C++ compiler (e.g., `sudo apt install cmake unzip build-essential`)
+
+
+Install
+-------
+
+Running `setup.sh` installs miniconda with the required python libraries, downloads third-party C++ libraries, and builds the C++ code.
--- setup.sh
+++ setup.sh
@@ -1,4 +1,3 @@
-sudo apt update
 sudo apt install cmake emacs build-essential unzip
 
 wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh
@@ -11,4 +10,15 @@ conda create -y -n test_env python=3.8
 conda activate test_env
 conda install --yes --file requirements.txt
 
-source setup_ext.sh
+cd cpp
+git clone https://github.com/pybind/pybind11.git
+wget https://gitlab.com/libeigen/eigen/-/archive/3.3.9/eigen-3.3.9.zip -O eigen.zip
+git clone https://github.com/autodiff/autodiff.git
+unzip -q eigen.zip
+rm eigen.zip
+mv eigen-3.3.9 eigen
+
+sudo apt install cmake
+cmake .
+make
+cd ../

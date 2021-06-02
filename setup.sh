dfce0b17799e103d7d19436bef882da2af610c65
Alkis Gotovos <alkisg@mit.edu>
2021-06-02 16:27:24 -0400

    Fix problem with autodiff flags

--- setup.sh
+++ setup.sh
@@ -4,14 +4,17 @@ source "./miniconda/etc/profile.d/conda.sh"
 conda config --set always_yes yes --set changeps1 no
 conda update -q conda
 conda info -a
-conda create -y -n test_env python=3.8
+conda create -y -n test_env python=3.7
 conda activate test_env
 conda install --yes --file requirements.txt
 
 cd cpp
 git clone https://github.com/pybind/pybind11.git
 wget https://gitlab.com/libeigen/eigen/-/archive/3.3.9/eigen-3.3.9.zip -O eigen.zip
-git clone https://github.com/autodiff/autodiff.git
+git clone https://github.com/autodiff/autodiff
+mv autodiff autodiff_tmp
+mv autodiff_tmp/autodiff .
+rm -rf autodiff_tmp
 unzip -q eigen.zip
 rm eigen.zip
 mv eigen-3.3.9 eigen

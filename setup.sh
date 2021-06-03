7e3e14d2bbae2a37473cd243d51c038c31928ecc
Alkis Gotovos <alkisg@mit.edu>
2021-06-03 09:08:26 -0400

    Small fix in setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 cd cpp
 git clone https://github.com/pybind/pybind11.git
-wget https://gitlab.com/libeigen/eigen/-/archive/3.3.9/eigen-3.3.9.tar.gz -O eigen.tar.gz
+wget https://gitlab.com/libeigen/eigen/-/archive/3.3.9/eigen-3.3.9.tar.gz
 git clone https://github.com/autodiff/autodiff
 mv autodiff autodiff_tmp
 mv autodiff_tmp/autodiff .
